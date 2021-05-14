64b64d9918363667f08ea2bb2dd71995f7adcefb
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-05-14 06:11:06 +0530

    Header done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <span className='header'>Movie Hub</span>
+    </div>
+  );
+};
+
+export default Header;
